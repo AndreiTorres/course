801fd405ab69b6bf006dc4fd7c93d7d8a9600357
AndreiTorres <andreitorres09@outlook.com>
2022-09-10 18:57:54 -0500

    Bowling Object Oriented

--- inclass/src/1_5_tdd/objectOriented/bowling.spec.ts
+++ inclass/src/1_5_tdd/objectOriented/bowling.spec.ts
@@ -8,4 +8,51 @@ describe('A bowling game', () => {
     game = new BowlingGame()
   });
 
+  it('can roll a ball', () => {
+    game = game.roll(0);
+  })
+
+  it('can play a game with all 0', () => {
+    game = rollMany(game, 20, 0);
+
+    expect( game.score() ).toBe(0);
+  })
+
+  it('can play a game with all 1', () => {
+    game = rollMany(game, 20, 1);
+
+    expect( game.score() ).toBe(20);
+  })
+
+  it('can roll a spare', () => {
+    game = game.roll(5).roll(5).roll(3);
+
+    game = rollMany(game, 17, 0);
+
+    expect( game.score() ).toBe(16);
+  })
+
+  it('can roll a strike', () => {
+    game = game.roll(10).roll(1).roll(1);
+    
+    game = rollMany(game, 16, 0);
+
+    expect( game.score() ).toBe(14);
+  })
+
+  it('can roll a perfect game', () => {
+    game = rollMany(game, 12, 10);
+
+    expect( game.score() ).toBe(300);
+  })
+
+
 });
+
+function rollMany(game: BowlingGame, rolls: number, pins: number)  {
+  for (let i = 0; i < rolls; i++) {
+    game = game.roll(pins);
+  }
+
+  return game;
+}
--- inclass/src/1_5_tdd/objectOriented/bowling.ts
+++ inclass/src/1_5_tdd/objectOriented/bowling.ts
@@ -1,5 +1,54 @@
 export class BowlingGame {
+    
+    private frames: number[];
 
+    constructor() {
+        this.frames = [];
+    }
 
-  
+    roll(pins: number): BowlingGame {
+        this.frames.push(pins);
+        return this;
+    }
+
+    private isStrike(index: number) {
+        return this.frames[index] === 10;
+    }
+
+    private isSpare(index: number) {
+        return this.frames[index] 
+            + this.frames[index + 1] === 10;
+    }
+
+    private sumNextTwoRolls(index: number) {
+        return 10 + this.frames[index + 1] + this.frames[index + 2];
+    }
+
+    private sumNextRoll(index: number) {
+        return 10 + this.frames[index + 2];
+    }
+
+    score(): number {
+
+        let score = 0;
+        let firstInFrame = 0;
+
+        for (let frame = 0; frame < 10; frame++) {
+            
+            if (this.isStrike(firstInFrame)) {
+                score += this.sumNextTwoRolls(firstInFrame);
+                firstInFrame++;
+            }
+            else if (this.isSpare(firstInFrame)) {
+                score += this.sumNextRoll(firstInFrame);
+                firstInFrame += 2;
+            } else {
+                score += this.frames[firstInFrame] + this.frames[firstInFrame + 1];
+                firstInFrame += 2;
+            }
+
+        }
+
+        return score;
+    }
 }
